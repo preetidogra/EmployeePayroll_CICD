ebc358afddf8fedd336194026a459ad095cb55da
preetidogra <70689452+preetidogra@users.noreply.github.com>
2021-09-24 17:24:31 +0200

    Add project files.

---
+++ Uppgift2_CICD/Admin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift1_CICD
+{
+    public class Admin
+    {
+        
+    }
+}
---
+++ Uppgift2_CICD/UserDetails.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Uppgift1_CICD
+{
+    class UserDetails
+    {
+    }
+}
+
+
+
+        
